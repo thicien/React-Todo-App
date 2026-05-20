482ba384ed9b9341bbac86542c0154becdf92f62
mugisha thicien <mugishathicien04@gmail.com>
2026-05-20 15:28:28 +0200

    Deleting files from a certain repository

--- node-learning/files.js
+++ node-learning/files.js
@@ -5,3 +5,35 @@ fs.readFile('./folder/ftp.txt', (err, data) => {
     }
     console.log(data.toString());
 });
+console.log('Why is this line running faster than the rest');
+
+fs.writeFile('./folder/ftp.txt', 'I am done with this', () => {
+    console.log('It a clear penalty obviously!');
+});
+fs.writeFile('./folder/tcp.txt', 'I will be promoted in next three years', () => {
+})
+
+if (!fs.existsSync('./components')) {
+   fs.mkdir('./components', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('The folder created successfully!')
+}) 
+} else {
+    fs.rmdir('./components', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder is deleted successfully');
+    })
+}
+
+if (fs.existsSync('./folder/delete.txt')) {
+    fs.unlink('./folder/delete.text', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('The file has deleted');
+    })
+}
