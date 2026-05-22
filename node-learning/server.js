cc42a88b1382806814612adb26e467329e748372
mugisha thicien <mugishathicien04@gmail.com>
2026-05-22 17:33:49 +0200

    Solved the issue

--- node-learning/server.js
+++ node-learning/server.js
@@ -1,25 +1,42 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-app.use(express.json());
-const users = [
-    { id: 1, name: 'Thicien' },
-    { id: 2, name: 'MUGISHA' }
-]
-app.get('/users', (req, res) => {
-    res.json(users);
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
+// app.use(express.json());
+// const users = [
+//     { id: 1, name: 'Thicien' },
+//     { id: 2, name: 'MUGISHA' }
+// ]
+// app.get('/users', (req, res) => {
+//     res.json(users);
+// })
+// app.post('/users', (req, res) => {
+//     const newUser = {
+//         id: users.length + 1,
+//         name: req.body.name
+//     };
+//     users.push(newUser);
+//     res.status(200).json({
+//         message: 'The new uses added successfully',
+//         name: newUser
+//     })
+// })
+// app.listen(PORT, () => {
+//     console.log(`My server is listening to this port ${PORT}`);
+// })
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log(req);
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('My server is listening to the port: 3000')
 })
-app.post('/users', (req, res) => {
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name
-    };
-    users.push(newUser);
-    res.status(200).json({
-        message: 'The new uses added successfully',
-        name: newUser
-    })
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+console.log(req)
 })
-app.listen(PORT, () => {
-    console.log(`My server is listening to this port ${PORT}`);
+server.listen(3000, 'localhost', () => {
+    console.log('My server is listening for port: 3000');
 })
