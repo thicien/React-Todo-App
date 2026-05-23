a8814ca88d68c439c42bc276a6aac6ebcddd24ae
mugisha thicien <mugishathicien04@gmail.com>
2026-05-23 18:55:51 +0200

    Fixing the bugs

--- node-learning/server.js
+++ node-learning/server.js
@@ -28,7 +28,6 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req);
 });
-
 server.listen(3000, 'localhost', () => {
     console.log('My server is listening to the port: 3000')
 })
