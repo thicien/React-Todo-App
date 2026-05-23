7944a90cf26cc942e7c9213f3a2bd71b8ebda292
mugisha thicien <mugishathicien04@gmail.com>
2026-05-23 18:57:32 +0200

    Adding the server creation

--- node-learning/server.js
+++ node-learning/server.js
@@ -1,28 +1,29 @@
-// const express = require('express');
-// const app = express();
-// const PORT = 3000;
-// app.use(express.json());
-// const users = [
-//     { id: 1, name: 'Thicien' },
-//     { id: 2, name: 'MUGISHA' }
-// ]
-// app.get('/users', (req, res) => {
-//     res.json(users);
-// })
-// app.post('/users', (req, res) => {
-//     const newUser = {
-//         id: users.length + 1,
-//         name: req.body.name
-//     };
-//     users.push(newUser);
-//     res.status(200).json({
-//         message: 'The new uses added successfully',
-//         name: newUser
-//     })
-// })
-// app.listen(PORT, () => {
-//     console.log(`My server is listening to this port ${PORT}`);
-// })
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+const users = [
+    { id: 1, name: 'Thicien' },
+    { id: 2, name: 'MUGISHA' }
+]
+
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name
+    };
+    users.push(newUser);
+    res.status(200).json({
+        message: 'The new uses added successfully',
+        name: newUser
+    })
+})
+app.listen(PORT, () => {
+    console.log(`My server is listening to this port ${PORT}`);
+})
 
 const http = require('http');
 const server = http.createServer((req, res) => {
