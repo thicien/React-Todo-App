dfb92be389c3462e9cd59fa9e1800e2baf6acd7c
mugisha thicien <mugishathicien04@gmail.com>
2026-06-07 09:32:56 +0200

    connecting the mongoose for efficient use of their ports

--- node-learning/server.js
+++ node-learning/server.js
@@ -1,49 +1,49 @@
-// const express = require('express');
-// const app = express();
-// const PORT = 3000;
-// app.use(express.json());
-// const users = [
-//     { id: 1, name: 'Thicien' },
-//     { id: 2, name: 'MUGISHA' }
-// ]
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+const users = [
+    { id: 1, name: 'Thicien' },
+    { id: 2, name: 'MUGISHA' }
+]
 
-// app.get('/users', (req, res) => {
-//     res.json(users);
-// })
-// app.post('/users', (req, res) => {
-//     const newUser = {
-//         id: users.length + 1,
-//         name: req.body.name
-//     };
-//     users.push(newUser);
-//     res.status(200).json({
-//         message: 'The new uses added successfully',
-//         name: newUser
-//     })
-// })
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name
+    };
+    users.push(newUser);
+    res.status(200).json({
+        message: 'The new uses added successfully',
+        name: newUser
+    })
+})
 
-// app.listen(PORT, () => {
-//     console.log(`My server is listening to this port ${PORT}`);
-// })
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-//     console.log(req);
-// });
-// server.listen(3000, 'localhost', () => {
-//     console.log('My server is listening to the port: 3000')
-// })
+app.listen(PORT, () => {
+    console.log(`My server is listening to this port ${PORT}`);
+})
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log(req);
+});
+server.listen(3000, 'localhost', () => {
+    console.log('My server is listening to the port: 3000')
+})
 
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-//     console.log(req.url, req.method);
-//     res.setHeader('Content-Type', 'text/html');
-//     res.write
-// })
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    res.setHeader('Content-Type', 'text/html');
+    res.write
+})
 
-// server.listen(3000, 'localhost', () => {
-//     console.log('My server is listening for port: 3000');
-// })
-// const http
+server.listen(3000, 'localhost', () => {
+    console.log('My server is listening for port: 3000');
+})
+const http
 
 const express = require('express');
 const mongoose = require('mongoose');
