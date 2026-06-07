cbcb2d1fec8847bdf203d8679731426b4d6aa43c
mugisha thicien <mugishathicien04@gmail.com>
2026-06-07 09:27:55 +0200

    Solving the issue with the listening ports

--- node-learning/app.js
+++ node-learning/app.js
@@ -5,11 +5,9 @@ app.listen(3000);
 app.get('/', (req, res) => {
     res.sendFile('./inde.html', { root: __dirname });
 })
-
 app.get('/index', (req, res) => {
     res.redirect('/')
 })
-
 app.use((req, res) => {
     res.status(404).sendFile('/', {root: __dirname})
 })
