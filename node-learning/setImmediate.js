99e70363a9c29bc136226a919cc92e7439ab36f5
mugisha thicien <mugishathicien04@gmail.com>
2026-06-17 15:17:32 +0200

    adding set immediate to the files

---
+++ node-learning/setImmediate.js
@@ -0,0 +1,39 @@
+console.log("1");
+
+setImmediate(() => {
+  console.log("2");
+});
+
+process.nextTick(() => {
+  console.log("3");
+});
+
+console.log("4");
+
+console.log("A");
+
+setImmediate(() => {
+  console.log("B");
+});
+
+process.nextTick(() => {
+  console.log("C");
+
+  process.nextTick(() => {
+    console.log("D");
+  });
+});
+
+console.log("E");
+
+console.log("Start");
+
+process.nextTick(() => {
+  console.log("nextTick");
+});
+
+setImmediate(() => {
+  console.log("immediate");
+});
+
+console.log("End");
